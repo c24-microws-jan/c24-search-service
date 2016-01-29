63e9c74b546a81208e5a9b3fb75125ec0666330d
H-P Dietz <hp@jandh.org>
2016-01-29 17:00:01 +0100

    Fix image mapping

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ const cdCache = require('./cdCache');
 cdCache.syncCDs();
 
 var cdController = require('./controller/cd.controller');
+
 // Define some default values if not set in environment
 const PORT = process.env.PORT || 3000;
 const SHUTDOWN_TIMEOUT = process.env.SHUTDOWN_TIMEOUT || 10000;
@@ -18,43 +19,47 @@ app.use(bodyParser.json());
 app.use(cors());
 
 // Add health check endpoint
-app.get(SERVICE_CHECK_HTTP, function (req, res) {
-  res.json({message: 'OK'});
+app.get(SERVICE_CHECK_HTTP, function(req, res) {
+  res.json({ message: 'OK' });
 });
 
 // Add all other service routes
-app.get('/', function (req, res) {
+app.get('/', function(req, res) {
   console.log('GET /');
   res.send('Hello Search Service');
 });
 
-app.get('/cd/:id', function (req, res, next) {
-  cdController.getCd(req.params.id, function (err, data) {
+app.get('/cd/:id', function(req, res, next) {
+  cdController.getCd(req.params.id, function(err, data) {
     if (err) {
-      return next(err)
+      return next(err);
     }
+
     if (!data.length) {
       return res.sendStatus(404);
     }
+
     res.json(data);
   });
 
 });
 
-app.get('/cd', function (req, res) {
-  cdController.getCds(req.query, function (err, data) {
+app.get('/cd', function(req, res) {
+  cdController.getCds(req.query, function(err, data) {
     if (err) {
-      return next(err)
+      return next(err);
     }
+
     res.json(data);
   });
 });
 
-app.get('/cd/most_recent', function (req, res) {
-  cdController.getMostRecentCds(req.params, function (err, data) {
+app.get('/cd/most_recent', function(req, res) {
+  cdController.getMostRecentCds(req.params, function(err, data) {
     if (err) {
-      return next(err)
+      return next(err);
     }
+
     res.json(data);
   });
 });
@@ -66,19 +71,19 @@ console.log(`Service listening on port ${PORT} ...`);
 
 ////////////// GRACEFUL SHUTDOWN CODE ////
 
-const gracefulShutdown = function () {
+const gracefulShutdown = function() {
   cdCache.destroy();
   console.log('Received kill signal, shutting down gracefully.');
   cdCache.destroy();
 
   // First we try to stop the server smoothly
-  server.close(function () {
+  server.close(function() {
     console.log('Closed out remaining connections.');
     process.exit();
   });
 
   // After SHUTDOWN_TIMEOUT we will forcefully kill the process in case it's still running
-  setTimeout(function () {
+  setTimeout(function() {
     console.error('Could not close connections in time, forcefully shutting down');
     process.exit();
   }, SHUTDOWN_TIMEOUT);
